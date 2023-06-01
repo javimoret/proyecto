d046b04ebb50f0812a2cc9b1ae75dd350f7ff1de
javi <javimm127@gmail.com>
2023-06-01 12:40:44 +0200

    quinto commit

---
+++ menu.php
@@ -0,0 +1,42 @@
+        <div id="logo">
+				<div id="logo2"></div>
+				<div id="texto">
+					<h1><strong>DEPORTES AL DIA</strong></h1>
+				</div>
+			</div>
+			<div class="menu">
+				<ul class="menu">
+					<li><a href="#arriba">Home</a>
+						<ul class="submenu">
+							<li><a href="#">Pagina 1</a></li>
+							<li><a href="#">Pagina 2</a></li>
+							<li><a href="#">Pagina 3</a></li>
+					</li>
+				</ul>
+				<li><a href="#">Features</a></li>
+
+				<li><a href="#">Pages</a>
+					<ul class="submenu">
+						<li><a href="#">Pagina 1</a></li>
+						<li><a href="#">Pagina 2</a></li>
+						<li><a href="#">Pagina 3</a></li>
+				</li>
+				</ul>
+
+				<li><a href="#">Gallery</a>
+					<ul class="submenu">
+						<li><a href="#">Pagina 1</a></li>
+						<li><a href="#">Pagina 2</a></li>
+						<li><a href="#">Pagina 3</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Blog</a>
+					<ul class="submenu">
+						<li><a href="#">Pagina 1</a></li>
+						<li><a href="#">Pagina 2</a></li>
+						<li><a href="#">Pagina 3</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Contacto</a></li>
+				</ul>
+			</div>
